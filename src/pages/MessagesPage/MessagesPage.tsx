099e531d56b6b9df3a31f6e1983015c5ab91177b
Seungwoo Kim <svvoo31o@gmail.com>
2024-04-29 17:07:13 +0200

    Feat: error

--- src/pages/MessagesPage/MessagesPage.tsx
+++ src/pages/MessagesPage/MessagesPage.tsx
@@ -7,6 +7,7 @@ import { Container } from "./style";
 import { AuthContextProvider } from "../../context/AuthContext";
 import { SocketContextProvider } from "../../context/SocketContext";
 
+//test
 const MessagesPage: React.FC = () => {
   return (
     <>
