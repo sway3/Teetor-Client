ea096ea59a5cf6212d63cf565ace47c10bec3a07
Seungwoo Kim <svvoo31o@gmail.com>
2024-04-29 10:09:03 +0200

    Feat: socket

--- src/utils/axiosInterceptor/axiosInterceptor.ts
+++ src/utils/axiosInterceptor/axiosInterceptor.ts
@@ -2,10 +2,11 @@ import axios, { AxiosInstance, InternalAxiosRequestConfig } from "axios";
 
 const axiosInstance: AxiosInstance = axios.create({
   baseURL: import.meta.env.VITE_BASE_URL,
-  withCredentials: true,
 });
 
 axiosInstance.interceptors.request.use((req: InternalAxiosRequestConfig) => {
+  req.withCredentials = true;
+  req.headers["Access-Control-Allow-Origin"] = "*";
   return req;
 });
 
